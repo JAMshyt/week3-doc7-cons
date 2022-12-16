aee4aa3f9d8c7643ace91c761c9843fba40faf02
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-16 18:11:22 +0300

    Add files via upload

---
+++ Строки 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Строки_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            StringBuilder text = new StringBuilder(Console.ReadLine());
+            Console.Write("Введите что заменить: ");
+            string subst1 = Console.ReadLine();
+            Console.Write("Введите на что заменить: ");
+            string subst2 = Console.ReadLine();
+            text.Replace(subst1, subst2);
+            Console.Write("Изменённая строка: "+text.ToString());
+        }
+    }
+}
---
+++ Строки 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Строки_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string[] text = Console.ReadLine().Split(' ', '.', ',', ':', ';', '!', '?'); ;
+            string[] result = new string[text.Length];
+            bool flag = false;
+            string res = "";
+            int max = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Length > max)
+                {
+                    max = text[i].Length;
+                    result[i] = text[i];
+                    res = text[i];
+                }
+                if (text[i].Length == max)
+                {
+                    result[i] = text[i];
+                }
+            }
+            Console.Write("Самое длинное слово: ");
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (res.Length >= result[i].Length)
+                {
+                    flag = true;
+                    break;
+                }
+                else
+                {
+                    Console.Write(result[i] + " ");
+                }
+            }
+            if (flag)  Console.Write(res);
+            Console.Write("\n");
+        }
+    }
+}
